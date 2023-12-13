05fbcff2f24422aefb8acb7d8daceaca2097ef99
Clkwong3 <134983608+Clkwong3@users.noreply.github.com>
2023-12-13 03:40:10 -0800

    Work - added another sample to test the links

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -4,16 +4,23 @@ import WorkItem from "./WorkItem";
 // Array of objects
 const data = [
   {
-    year: 2023,
+    year: 2020,
     title: "Example 1",
-    duration: "5+ Years (Ongoing)",
+    duration: "3+ Years (Ongoing)",
     details:
-      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam in leo pellentesque diam aliquam rutrum non vitae lorem. Maecenas dapibus nibh ac ipsum mollis imperdiet.",
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam vel placerat mi. Praesent vulputate finibus est, non auctor tortor finibus quis. Sed posuere sit amet sapien at mattis. Nunc nisi libero, congue eu nunc auctor, mollis blandit metus. Ut at lectus porta, finibus leo congue, finibus enim. In sed tincidunt urna. Morbi lectus erat, dignissim id efficitur non, sagittis nec lectus. Sed molestie lectus malesuada.",
   },
   {
-    year: 2020,
+    year: 2017,
     title: "Sample 2",
-    duration: "3 Years",
+    duration: "2 Years",
+    details:
+      "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce sed sollicitudin tellus. Sed gravida metus et dui blandit, sit amet varius velit vehicula. Vestibulum aliquam elit quam, sollicitudin iaculis ipsum vulputate id. Nunc et mi elit. Pellentesque habitant morbi tristique senectus et netus et malesuada.",
+  },
+  {
+    year: 2013,
+    title: "Tibit 3",
+    duration: "4 Years",
     details:
       "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed urna diam, lobortis quis luctus non, egestas sed dui. Maecenas libero libero, suscipit sed blandit vitae, rhoncus cursus ligula. Pellentesque habitant morbi tristique senectus et netus.",
   },
